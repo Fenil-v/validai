a1c718939e2761b419b251c02870d49e1dcff425
Fenil-Mansukhbhai-Vaghasiya <vaghasiya.f@northeastern.edu>
2025-02-16 14:20:39 -0500

    changed redis connection

--- backend/config/redis.py
+++ backend/config/redis.py
@@ -1,7 +1,10 @@
+
 # type: ignore
 import aioredis 
 
 REDIS_URL = "redis://localhost:6379"
 
 async def get_redis():
-    return await aioredis.from_url(REDIS_URL, decode_responses=True)
+    # Create a Redis client using aioredis 2.x
+    redis = await aioredis.from_url(REDIS_URL, decode_responses=True)
+    return redis
