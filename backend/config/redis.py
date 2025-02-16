ca71549dc378bb20218287df6a5f4f2bc07356f3
Fenil-Mansukhbhai-Vaghasiya <vaghasiya.f@northeastern.edu>
2025-02-16 14:29:02 -0500

    changed redis connection

--- backend/config/redis.py
+++ backend/config/redis.py
@@ -2,5 +2,5 @@ import redis
 import asyncio
 
 async def get_redis():
-    redis = await asyncio.to_thread(redis.asyncio.Redis, host='localhost', port=6379, db=0)
-    return redis
+    redis_client = await asyncio.to_thread(redis.asyncio.Redis, host='localhost', port=6379, db=0)
+    return redis_client
